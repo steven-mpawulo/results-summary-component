c5155e8e4b0c456831542e9d69cdbcd9e2ac2bea
steven-mpawulo <stevenmpawulo@gmail.com>
2023-07-17 11:44:42 +0300

    works on first-container styles

--- src/components/ResultsSummaryComponent.js
+++ src/components/ResultsSummaryComponent.js
@@ -3,41 +3,41 @@ import IconMemory from './IconMemory'
 import IconReaction from './IconReaction'
 import IconVerbal from './IconVerbal'
 import IconVisual from './IconVisual'
+import '../styles/results_summary_component.css'
 
 const ResultsSummaryComponent = () => {
     return (
         <main>
             <div className="main-container">
                 <div className="first-container">
-                    <p>Your Result</p>
+                    <p className = "first-text">Your Result</p>
                     <div className="result-container">
-                        <p>76</p>
-                        <p>of</p>
-                        <p>100</p>
+                        <p className='number'>76</p>
+                        <p>of 100</p>
                     </div>
-                    <p>Great
-                        <p>You have scored higher than 65% of the people that have taken this tests.</p>
-                    </p>
+                    <p className='second-text'>Great</p>
+                    <p className='third-text'>You have scored higher than 65% of people who have taken these tests</p>
+
                 </div>
-                <div className="seconf-container">
+                <div className="second-container">
                     <p>Summary</p>
                     <div className="reaction-container">
-                        <IconReaction/>
+                        <IconReaction />
                         <p>Reaction</p>
                         <p>80/100</p>
                     </div>
                     <div className="memory-container">
-                        <IconMemory/>
+                        <IconMemory />
                         <p>Memory</p>
                         <p>92/100</p>
                     </div>
                     <div className="verbal-container">
-                        <IconVerbal/>
+                        <IconVerbal />
                         <p>Verbal</p>
                         <p>61/100</p>
                     </div>
                     <div className="visual-container">
-                        <IconVisual/>
+                        <IconVisual />
                         <p>Visual</p>
                         <p>72/100</p>
                     </div>
